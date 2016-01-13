16410e2c1b5d6af7243c6cf7eb30bc0d7d8d4f79
rwojnarowski <radziu92@gmail.com>
2016-01-13 17:37:25 +0100

    Stripe payment, views

---
+++ app/models/image.rb
@@ -0,0 +1,13 @@
+class Image < ActiveRecord::Base
+  belongs_to :user
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      erros.add(:picture, "should be less than 5MB")
+    end
+  end
+end
